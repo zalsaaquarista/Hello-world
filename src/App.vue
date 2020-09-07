e7fc37ba12b8268bde476d94a2f5fc9689ddd2fa
Zalsa Aquarista <zalsa_aquarista_27rpl@student.smktelkom-mlg.sch.id>
2020-09-07 23:06:14 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <First msg="First Component"></First>
+  </div>
+</template>
+
+<script>
+import First from './components/First.vue'
+
+export default {
+  name: 'App',
+  components: {
+    First
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
